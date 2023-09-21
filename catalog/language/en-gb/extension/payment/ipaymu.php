58a5056de06b527673e2c3cd6063be31c35875bf
putuedi <67893399+putuedi@users.noreply.github.com>
2023-09-21 16:50:40 +0800

    Update ipaymu.php

--- catalog/language/en-gb/extension/payment/ipaymu.php
+++ catalog/language/en-gb/extension/payment/ipaymu.php
@@ -1,6 +1,6 @@
 <?php
 // Text
 $_['text_title'] = 'iPaymu';
-$_['currency_support'] = "iPaymu only support IDR, please contact administrator";
+$_['currency_support'] = "iPaymu only support IDR, please contact administrator support@ipaymu.com";
 $_['currency_convert'] = "Your Order will convert into IDR, please confirm to continue.";
 ?>
